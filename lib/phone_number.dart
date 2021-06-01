c39b8ccfb4cb64929ad51fd8c65a08fe5d0b4b61
Mafahir Mohammed <mafahir.mohammed@v-key.com>
2021-06-01 14:16:33 +0800

    added factory method to parse phone number from a complete number

--- lib/phone_number.dart
+++ lib/phone_number.dart
@@ -1,3 +1,5 @@
+import 'package:intl_phone_field/countries.dart';
+
 class PhoneNumber {
   String? countryISOCode;
   String? countryCode;
@@ -11,5 +13,31 @@ class PhoneNumber {
 
   String get completeNumber {
     return countryCode! + number!;
+  }
+
+  factory PhoneNumber.fromCompleteNumber(String completeNumber) {
+    final number = completeNumber.replaceFirst(RegExp(r'^(\+|0+)'), '');
+    final country = countries.firstWhere((map) {
+      final dialCode = map['dial_code'];
+      final len = map['max_length'];
+      if (!number.startsWith(dialCode.toString())) {
+        return false;
+      }
+      final completeLength = len + dialCode.toString().length;
+      if (number.length != completeLength) {
+        return false;
+      }
+      return true;
+    }, orElse: () => Map<String, dynamic>());
+    if (country.isNotEmpty) {
+      final code = country['code'];
+      final dialCode = country['dial_code'].toString();
+      return PhoneNumber(
+          countryISOCode: code,
+          countryCode: dialCode,
+          number: number.substring(dialCode.length));
+    } else {
+      return PhoneNumber(countryISOCode: null, countryCode: null, number: null);
+    }
   }
 }
